09dcc9a55a617517036a65c099c849b2082d2315
Que1Pereza2 <125260665+Que1Pereza2@users.noreply.github.com>
2024-01-14 23:29:33 +0100

    rollback

--- src/main/java/com/example/demo/model/Marcas.java
+++ src/main/java/com/example/demo/model/Marcas.java
@@ -1,16 +1,19 @@
-/**
- * Pre:---
- * Post: Clase de entidad que representa la tabla "marcas" en la base de datos.
- */
 package com.example.demo.model;
 
 import java.util.List;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "marcas", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "nombre")
+@UniqueConstraint(columnNames = "nombre")
 })
 public class Marcas {
 
@@ -19,58 +22,28 @@ public class Marcas {
     private Long id;
 
     private String nombre;
-
+    
     @OneToMany(mappedBy = "marcas", cascade = CascadeType.ALL)
     private List<Modelos> modelos;
     
     @OneToMany(mappedBy="marcas", cascade=CascadeType.ALL, orphanRemoval = true)
     private List<Vehiculos> vehiculos;
 
-    /**
-     * Pre:---
-     * Post: Obtiene el ID de la marca.
-     */
+    // Getters and setters
+
     public Long getId() {
         return id;
     }
 
-    /**
-     * Pre:---
-     * Post: Establece el ID de la marca.
-     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    /**
-     * Pre:---
-     * Post: Obtiene el nombre de la marca.
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * Pre:---
-     * Post: Establece el nombre de la marca.
-     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    /**
-     * Pre:---
-     * Post: Obtiene la lista de modelos asociados a la marca.
-     */
-    public List<Modelos> getModelos() {
-        return modelos;
-    }
-
-    /**
-     * Pre:---
-     * Post: Establece la lista de modelos asociados a la marca.
-     */
-    public void setModelos(List<Modelos> modelos) {
-        this.modelos = modelos;
     }
 }
--- src/main/java/com/example/demo/repository/MarcasRepository.java
+++ src/main/java/com/example/demo/repository/MarcasRepository.java
@@ -1,12 +1,9 @@
-/**
- * Pre:---
- * Post: Interfaz de repositorio que extiende JpaRepository para realizar operaciones CRUD en la entidad "Marcas".
- */
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.model.Marcas;
 
-public interface MarcasRepository extends JpaRepository<Marcas, Long> {
-    // Operaciones CRUD básicas proporcionadas por JpaRepository
+public interface MarcasRepository extends JpaRepository <Marcas, Long> {
+	//Operaciones CRUD básicas que vienen a través de JpaRepository
 }
